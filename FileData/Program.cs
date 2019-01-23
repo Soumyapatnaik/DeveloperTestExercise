34ab6301954d65b7aa8192a37f34d11df6f0d154
Soumyapatnaik <46927661+Soumyapatnaik@users.noreply.github.com>
2019-01-23 08:57:26 +0530

    Add files via upload

---
+++ FileData/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ThirdPartyTools;
+
+namespace FileData
+{
+    public static class Program
+    {
+        
+        public static void Main(string[] args)
+        {            
+            
+            try
+            {
+                Boolean validValues;
+
+                validValues = FileVersionAndSizeCheck(args[0], args[1]);
+                
+                if (validValues == false)
+                {
+                    Console.WriteLine("Wrong Input");
+                    Console.ReadLine();
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occurred: ", e);
+            }
+
+        }
+        public static Boolean FileVersionAndSizeCheck(string Functionality, string FileName)
+        {
+            Boolean validValues = false;
+            try
+            {
+                
+                string[] FileVersionValues = System.Configuration.ConfigurationSettings.AppSettings["FileVersion"].Split(',');
+
+                FileDetails filedetails = new FileDetails();
+
+                for (int fileVerCount = 0; fileVerCount < FileVersionValues.Length; fileVerCount++)
+                {
+                    if (Functionality.Equals(FileVersionValues[fileVerCount]))
+                    {
+                        Console.WriteLine(filedetails.Version(FileName));
+                        validValues = true;                       
+                        break;
+                        
+                    }
+                }
+
+                string[] FileSizeValues = System.Configuration.ConfigurationSettings.AppSettings["FileSize"].Split(',');
+
+                
+                for (int fileSizeCount = 0; fileSizeCount < FileSizeValues.Length; fileSizeCount++)
+                {
+                    if (Functionality.Equals(FileSizeValues[fileSizeCount]))
+                    {
+                        Console.WriteLine(filedetails.Size(FileName));
+                        validValues = true;
+                        break;
+
+                    }
+
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occurred: ", e);
+            }
+            return validValues;
+
+        }
+        
+    }
+}
---
+++ FileDataTests/ProgramTests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FileData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileData.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod]        
+        public void MainTest()
+        {
+           Program.FileVersionAndSizeCheck("-v", "c:/test.txt");            
+            
+        }
+        public void MainTest1()
+        {
+            Program.FileVersionAndSizeCheck("--v", "c:/test.txt");
+
+        }
+        public void MainTest2()
+        {
+            Program.FileVersionAndSizeCheck("/v", "c:/test.txt");
+
+        }
+        public void MainTest3()
+        {
+            Program.FileVersionAndSizeCheck("version", "c:/test.txt");
+
+        }
+        public void MainTest4()
+        {
+            Program.FileVersionAndSizeCheck("-s", "c:/test.txt");
+
+        }
+        public void MainTest5()
+        {
+            Program.FileVersionAndSizeCheck("--s", "c:/test.txt");
+
+        }
+        public void MainTest6()
+        {
+            Program.FileVersionAndSizeCheck("/s", "c:/test.txt");
+
+        }
+        public void MainTest7()
+        {
+            Program.FileVersionAndSizeCheck("size", "c:/test.txt");
+
+        }
+
+
+    }
+}
